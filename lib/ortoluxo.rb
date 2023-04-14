30bff68267f11c3f8794ce1d63a6fbc41fbd47a8
Nathanael Vieira <59123681+NathanaelV@users.noreply.github.com>
2023-04-14 19:03:01 -0300

    Create filter

--- lib/ortoluxo.rb
+++ lib/ortoluxo.rb
@@ -7,5 +7,20 @@ require_relative 'ortoluxo/version'
 
 module Ortoluxo
   class Error < StandardError; end
-  # Your code goes here...
+
+  class F1SalesCustom::Hooks::Lead
+    class << self
+      def switch_source(lead)
+        source_name = lead.source.name
+        message_down = lead.message&.downcase || ''
+
+        return "#{source_name} - Corifeu" if message_down['av. corifeu de azevedo']
+        return "#{source_name} - Braz Leme" if message_down['av. braz leme']
+        return "#{source_name} - Autonomistas" if message_down['av. dos autonomistas']
+        return "#{source_name} - Otto Baumgart" if message_down['av. otto baumgart']
+
+        source_name
+      end
+    end
+  end
 end
